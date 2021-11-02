8b78312946f1894295effc895cfb867a82b0ea22
um.kyungil <umkyungil@gmail.com>
2021-11-02 19:48:07 +0900

    Updates

---
+++ precache-manifest.1590167e90074272e960b78b6bd3d393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f84fc82031396af470eced51707939",
+    "url": "/umkyungil/furuoka-drug-twitter/index.html"
+  },
+  {
+    "revision": "b7f0ad80b37280358b4b",
+    "url": "/umkyungil/furuoka-drug-twitter/static/css/main.55bfdf87.chunk.css"
+  },
+  {
+    "revision": "13a74ad9ac69309a269a",
+    "url": "/umkyungil/furuoka-drug-twitter/static/js/2.e82037cd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/umkyungil/furuoka-drug-twitter/static/js/2.e82037cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f0ad80b37280358b4b",
+    "url": "/umkyungil/furuoka-drug-twitter/static/js/main.e33f606e.chunk.js"
+  },
+  {
+    "revision": "4f1a6defa62a3b1f7bd8",
+    "url": "/umkyungil/furuoka-drug-twitter/static/js/runtime-main.04ef7c14.js"
+  }
+]);
